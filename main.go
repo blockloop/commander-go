e0c0ce51b17302c9cae1103eb5f300e51d0857da
Brett Jones <bjones027@gmail.com>
2016-01-02 21:14:57 -0600

    parse optional, range, required

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 	"regexp"
+	"strings"
 )
 
 var (
-	shortFlagReg = regexp.MustCompile(`^-[A-Za-z0-9]`)
-	longFlagReg  = regexp.MustCompile(`^--[A-Za-z0-9-]+`)
-	// flagMatchReg = regexp.MustCompile(`^(-\w)?(, )?(--[a-zA-Z]+)?( )?([<\[]\w+[\]>])?$`)
-	flagMatchReg = regexp.MustCompile(`^(-\w)?(, )?(--[a-zA-Z-]+)?( )?([<\[]\w+[\]>])?(.*)$`)
+	flagMatchReg = regexp.MustCompile(`^(-\w)?(, )?(--[a-zA-Z0-9-]+)?( )?(.+)?$`)
+
+	options = make([]*option, 0)
 )
 
 // Commander is the type used
@@ -20,13 +20,9 @@ func (c *Commander) OptionString(flags string, desc string, parser func(interfac
 	if !flagMatchReg.MatchString(flags) {
 		log.Panicln("pattern '", flags, "' is not in the correct format")
 	}
-	short, long, extra := parseFlags(flags)
-	// fmt.Printf("ORIGINAL: '%s'\n", flags)
-	// fmt.Println("SHORT:", short)
-	// fmt.Println("LONG:", long)
-	// fmt.Println("EXTRA:", extra)
-	// fmt.Println("---")
-	// fmt.Println("---")
+	opt := newOption(parseFlags(flags))
+
+	options = append(options, opt)
 }
 
 func main() {
@@ -37,11 +33,37 @@ func main() {
 	c.OptionString("-d <dir>", "desc", nil, "")
 	c.OptionString("--directory <dir>", "desc", nil, "")
 	c.OptionString("-d, --directory <dir>", "desc", nil, "")
+	c.OptionString("-d, --directory <dir>..", "desc", nil, "")
 	c.OptionString("-d, --directory [dir]", "desc", nil, "")
 	c.OptionString("-d, --directory-name [dir]", "desc", nil, "")
 }
 
-func parseFlags(flags string) (short string, long string, extra []string) {
+func parseFlags(flags string) (short string, long string, extra string) {
 	m := flagMatchReg.FindAllStringSubmatch(flags, -1)[0]
-	return m[1], m[3], m[5:]
+	return m[1], m[3], m[5]
+}
+
+type option struct {
+	HasValue   string
+	IsOptional bool
+	IsRange    bool
+	IsRequired bool
+	LongFlag   string
+	ShortFlag  string
+}
+
+func newOption(short string, long string, extra string) *option {
+	opt := &option{ShortFlag: short, LongFlag: long}
+
+	if strings.IndexRune(extra, '[') == 0 {
+		opt.IsOptional = true
+	} else if strings.IndexRune(extra, '<') == 0 {
+		opt.IsRequired = true
+	}
+
+	if strings.Index(extra, "...") != -1 {
+		opt.IsRange = true
+	}
+
+	return opt
 }
